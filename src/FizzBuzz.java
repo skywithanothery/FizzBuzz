31523ca7fdae072e14637c5b42593142743fdc27
skyy <jiayinglee0@gmail.com>
2024-05-16 19:02:13 -0400

    refactored to extract helper method

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -4,33 +4,28 @@
 class FizzBuzz {
 
     public static void main(String[] args) {
+        int j = 1;
+        while (j < 100) {
 
-        for (int i = 1; i < 100; i++) {
-            //testing
-
-
-            // Find out which numbers divide i.
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
-
-            // Print our appropriate result.
-            if (divisibleBy3 && divisibleBy5) {
-
-                System.out.println("Fizz Buzz");
-
-            } else if (divisibleBy3) {
-
-                System.out.println("Fizz");
-
-            } else if (divisibleBy5) {
-
-                System.out.println("Buzz");
-
-            } else {
-
-                System.out.println(i);
-
-            }
+            j = doFizzBuzz(j);
         }
+    }
+
+    private static int doFizzBuzz(int j) {
+        boolean divisibleBy3 = j % 3 == 0;
+        boolean divisibleBy5 = j % 5 == 0;
+
+        if (divisibleBy3 && divisibleBy5) {
+
+            System.out.println("Fizz Buzz");
+        } else if (divisibleBy3) {
+            System.out.println("Fizz");
+        } else if (divisibleBy5) {
+            System.out.println("Buzz");
+        } else {
+            System.out.println(j);
+        }
+        j++;
+        return j;
     }
 }
